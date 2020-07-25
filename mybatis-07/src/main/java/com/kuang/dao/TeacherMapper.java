56fe465c3f2d021f65fb91fec8ff0d3e1a3fa549
Fangxin Wang <2681086480@qq.com>
2020-07-25 15:51:51 +0800

    Mybatis多对一和一对多映射

--- mybatis-07/src/main/java/com/kuang/dao/TeacherMapper.java
+++ mybatis-07/src/main/java/com/kuang/dao/TeacherMapper.java
@@ -4,10 +4,9 @@ import com.kuang.pojo.Teacher;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
-import java.util.List;
-
 public interface TeacherMapper {
 
-    @Select("select * from teacher where id = #{id}")
+    // 获取指定老师下的所有学生
     Teacher getTeacher(@Param("id") int id);
+    Teacher getTeacher2(@Param("id") int id);
 }
